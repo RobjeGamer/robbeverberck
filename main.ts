2df259351d2269b4039523a492dd8c5a52ca29cb
RobjeGamer <robbe.verberck@gmail.com>
2025-03-20 11:28:17 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,32 +1,3 @@
-Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
 basic.forever(function () {
-    if (true) {
-    	
-    } else {
-    	
-    }
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        basic.showIcon(IconNames.No)
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
-        basic.clearScreen()
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # # # # #
-            . . # . .
-            . . # . .
-            `)
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
-        basic.pause(5000)
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
-        basic.clearScreen()
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
-        basic.pause(500)
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
-        basic.pause(500)
-        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-        basic.showIcon(IconNames.No)
-    }
+	
 })
